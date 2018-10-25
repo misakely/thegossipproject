42414f6352ea7922ad334fc2990678329dbf534e
Juliette Audema <juliette.audema@gmail.com>
2018-10-25 21:56:40 +0200

    Manque juste plusieurs recipients

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,16 @@
 require 'faker'
 
 10.times do |city|
-	cities = City.create!(city_name: Faker::GameOfThrones.city)
+	cities = City.create!(name: Faker::GameOfThrones.city)
 end
 
 10.times do |index|
-	users = User.create!(first_name: Faker::Superhero.name, last_name: Faker::TwinPeaks.character, mail: Faker::Internet.email, city_id: Faker::Number.between(1,10), age: Faker::Number.between(18,90))
+	users = User.create!(first_name: Faker::Superhero.name, last_name: Faker::TwinPeaks.character, description: Faker::TwinPeaks.quote, email: Faker::Internet.email, city_id: Faker::Number.between(1,10), age: Faker::Number.between(18,90))
   tags = Tag.create!(title: Faker::HarryPotter.book)
-  comments = Comment.create!(content: Faker::GreekPhilosophers.quote)
 end
 
 20.times do |index|
-  gossips = Gossip.create!(content: Faker::TwinPeaks.quote, title: Faker::Dessert.topping, user_id: )
+  gossips = Gossip.create!(content: Faker::TwinPeaks.quote, title: Faker::Dessert.topping, user_id: Faker::Number.between(1,10))
 end 
 
 1.times do |index|
